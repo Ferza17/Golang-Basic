4018cb3c4377539ae18a27970b2d7cd9407890f4
Ferza17 <feryreza85@gmail.com>
2020-12-01 15:25:50 +0700

    naming variable

--- zero values/main.go
+++ zero values/main.go
@@ -1,23 +1,31 @@
+/////////////////////////////////
+// Types and Zero Values
+// Go Playground: https://play.golang.org/p/zItROROXi64
+/////////////////////////////////
+
 package main
 
 import "fmt"
 
 func main() {
-	var a = 4
-	var b = 5.2
 
-	// Convert Variable
-	a = int(b)
-	fmt.Println(a, b)
+	// you must provide a type for each variable you declare or Go should be able to infer it
+	var a int = 10
+	var b = 15.5      // type inference (deduction)
+	c := "Gopher"     // short declaration, type inference
+	_, _, _ = a, b, c // Blank Identifier (_) to get rid of unused variable error
 
-	// var x int
-	// x = "5"
+	// Go is a Statically and Strong Typed Programming Language
+	// a = 3.14 -> error. A variable cannot change it's type
+	// a = b    -> error. It's not allowed to assign a type to another type
 
-	// Zero Values because it doesnt have a value that assign into the variable
-	var value int
-	var price float64
-	var name string
-	var done bool
+	//** ZERO VALUES **//
 
-	fmt.Print(value, price, name, done)
+	// An uninitialized variable or empty variable  will get the so called ZERO VALUE
+	// The zero-value mechanism of Go ensures that a variable always holds a well defined value of its type
+	var value int                         // initialized with 0
+	var price float64                     // initialized with 0.0
+	var name string                       // initialized with empty string -> ""
+	var done bool                         // initialized with false
+	fmt.Println(value, price, name, done) // -> 0 0.0 ""  false
 }
