193f3ffe22673fa967a2d350230da9863a871992
Fery Reza Aditya <3026317-Ferza17@users.noreply.gitlab.com>
2020-12-13 20:00:57 +0700

    update string in GO

--- strings_basics/main.go
+++ strings_basics/main.go
@@ -1,5 +1,35 @@
 package main
 
+import "fmt"
+
 func main() {
+	s1 := "Hey There Go!"
+	fmt.Println(s1)
+
+	fmt.Println("He Says: \"Hello!\"")
+
+	fmt.Println(`He Says: "Hello!"`)
+
+	s2 := `I Like \n Go!` // raw string
+	fmt.Println(s2)
+
+	fmt.Println("Price: 100\nBrand: Nike")
+	fmt.Println(`
+Price : 100
+Brand : Nike
+	`)
+
+	fmt.Println(`C:\Users\Fery`)
+	fmt.Println(`C:\\Users\\Fery`)
+
+	var s3 = "I Love " + "Go " + "Programming"
+	fmt.Println(s3 + "!")
+
+	fmt.Println("Element at index 0: ", s3[0]) // it printed 73 because I is 73 in ASCII
+
+	// s3[5] = 'x' // Error cannot assign
+
+	fmt.Printf("%s\n", s3)
+	fmt.Printf("%q\n", s3)
 
 }
